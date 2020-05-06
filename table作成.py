2f0f219ebce99bba4eaecfa921bc380f7c96fce5
yatomin <64619380+yatomin@users.noreply.github.com>
2020-05-06 11:19:08 +0900

    Update table作成.py

--- table作成.py
+++ table作成.py
@@ -8,10 +8,19 @@ conn = sqlite3.connect(DATABASE)
 now = datetime.datetime.now()
 
 def create_table():
-    sql = '''CREATE TABLE IF NOT EXISTS now.strftime("%Y_%m_%d")
-             (id INTEGER PRIMARY KEY,
-              app_url TEXT,
-              app_name TEXT,
-              store_url TEXT)'''
-    conn.execute(sql)
+    now = datetime.datetime.now()
+    now = now.strftime("%Y_%m_%d")
+    f ='''CREATE TABLE IF NOT EXISTS {now}
+             (code INTEGER PRIMARY KEY,
+              name TEXT,
+              sector TEXT,
+              date INTEGER,
+              start_price INTEGER,
+              high_price INTEGER,
+              low_price INTEGER,
+              MA25 INTEGER,
+              volume INTEGER)'''
+    conn.execute(f)
     conn.commit()
+if __name__ == '__main__':
+    create_table()
