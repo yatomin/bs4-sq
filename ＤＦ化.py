464e109c1b29128c68c14aab3612569308da24c2
yatomin <64619380+yatomin@users.noreply.github.com>
2020-05-09 20:18:11 +0900

    Add files via upload

---
+++ ＤＦ化.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+import sqlite3
+import pandas as pd
+
+dbname = "stocks.db"
+conn = sqlite3.connect(dbname)
+cur = conn.cursor()
+
+# dbをpandasで読み出す。
+df = pd.read_sql('SELECT * FROM brands', conn)
+
+print(df)
+
+cur.close()
