5af181657404c4782cbb405cedb602cbb90d4b14
yatomin <64619380+yatomin@users.noreply.github.com>
2020-05-11 15:26:11 +0900

    Update ＤＦ化.py

--- ＤＦ化.py
+++ ＤＦ化.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 import sqlite3
 import pandas as pd
+import datetime
 
+now = datetime.datetime.now()
 dbname = "stocks.db"
 conn = sqlite3.connect(dbname)
 cur = conn.cursor()
@@ -9,11 +11,17 @@ cur = conn.cursor()
 # dbをpandasで読み出す。
 df = pd.read_sql('SELECT * FROM brands', conn)
 
-print(df)
-
 cur.close()
 conn.close()
 
+df_v1 =df[df['volume'].str.contains('百万円')]
+df_r = df_v1.replace('百万円','', regex=True)
+df_r2 = df_r.replace(',','', regex=True)
+df_r3 = df_r2['volume'].str.rstrip()
+df_d =df.drop(columns=['volume'])
+df_a = pd.concat([df_d,df_r3],axis =1)
+df_v =df_a[df.values == '東証１']
+df_s1 = df_v[df_v['volume'] > 100]
+print(df_v)
 
-df_s = df.sort_values('market', ascending=False)
-print(df_s)
+#df_v.to_csv(now.strftime("%Y_%m_%d")+ ".csv",encoding="utf-8-sig")
